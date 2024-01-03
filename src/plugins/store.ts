7a9cb3d2cc89ca17f1fa9599e2e4791a158f7a97
Korin <1878464932@qq.com>
2024-01-03 12:04:11 +0800

    添加主唱

--- src/plugins/store.ts
+++ src/plugins/store.ts
@@ -13,7 +13,7 @@ export const useInfosStore = defineStore('infos', () => {
         "上海市": ["不限", "宝山区", "静安区", "嘉定区", "金山区", "青浦区", "崇明区", "普陀区", "徐汇区", "长宁区", "松江区", "闵行区", "虹口区", "奉贤区", "黄浦区", "浦东新区"],
         "暂未开放": ["暂未开放"],
     })
-    const Insts = ref(['电吉他','电贝斯','键盘', '架子鼓','木吉他'])
+    const Insts = ref(['电吉他','电贝斯','键盘', '架子鼓','木吉他','主唱'])
     const show_snackbar = ref()
     const snackbar_text = ref('')
     const show_nav = ref()
